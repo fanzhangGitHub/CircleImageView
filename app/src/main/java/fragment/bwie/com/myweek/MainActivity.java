271ad5a8032471b4837f75abb15701d27e83a031
fanzhangGitHub <2468242266@qq.com>
2016-12-05 11:03:52 +0800

    CircleIamge_button

--- app/src/main/java/fragment/bwie/com/myweek/MainActivity.java
+++ app/src/main/java/fragment/bwie/com/myweek/MainActivity.java
@@ -1,22 +1,21 @@
 package fragment.bwie.com.myweek;
 
-import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MotionEvent;
 import android.view.View;
-import android.widget.ImageButton;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
     //属性定义
-    private ImageButton mButton;
+    private ImageView mButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         //视图属性初始化
-        mButton = (ImageButton) findViewById(R.id.image_button);
+        mButton = (ImageView) findViewById(R.id.image_button);
         //视图添加点击事件
         mButton.setOnTouchListener(new View.OnTouchListener() {
             @Override
@@ -25,7 +24,6 @@ public class MainActivity extends AppCompatActivity {
                     case MotionEvent.ACTION_DOWN:
                        //点击事件
                         mButton.setBackgroundResource(R.mipmap.man);
-                        mButton.setColorFilter(Color.RED);
 
                         break;
                     case MotionEvent.ACTION_UP:
